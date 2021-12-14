5a0db1377908c4b5c831a112931876801c7fcc27
Namacha411 <thdyk.4.11@gmail.com>
2021-12-14 11:49:41 +0900

    SSHで実行できるCLIコマンドの実行 (#21)

--- auto-attendance-check/core/main.py
+++ auto-attendance-check/core/main.py
@@ -1,6 +1,26 @@
-def main():
-    pass
+import fire
+
+class Commands(object):
+    """
+    auto-attendance-checkのAPI
+    sshでCLI実行する目的
+
+    Note
+    ----
+    GUIの操作に対応させて必要なコマンドを作っていく
+    """
+    def AttendData():
+        pass
+
+    def TekePhoto():
+        pass
+
+    def SetTimeTable():
+        pass
+
+    def Configuration():
+        pass
 
 
 if __name__ == "__main__":
-    main()
+    fire.Fire(Commands)
