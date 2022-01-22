73b58e9bca4f1594f15ed8b72426dca7f377c432
Omusubi0123 <91716482+Omusubi0123@users.noreply.github.com>
2022-01-22 20:49:57 +0900

    画像のGUI画面表示と出席状況の閲覧 (#50)

--- auto-attendance-check/GUI/gui.py
+++ auto-attendance-check/GUI/gui.py
@@ -1,141 +1,139 @@
 # GUI main file
 # Use under Python3.8
 import tkinter as tk
-from tkinter.constants import (
-    NE,
-    NW,
-    RAISED,
-    X,
-    Y,
-)
+from tkinter.constants import (NE, NW, RAISED, X, Y,)
 import maniplation
 import sub_frame
 import owner
 
-# main window の作成
-window = tk.Tk()
-window.title("MANIPLATION")
-window.geometry("815x680")
-window.grid_rowconfigure(0, weight=1)
-window.grid_columnconfigure(0, weight=1)
+class main_frame:
+    def __init__(self):
+        # main window の作成
+        self.window = tk.Tk()
+        self.window.title("MANIPLATION")
+        self.window.geometry("815x680")
+        self.window.grid_rowconfigure(0, weight=1)
+        self.window.grid_columnconfigure(0, weight=1)
 
-""" main frame 作成部 """
+        """ main frame 作成部 """
 
-# main frame の作成
-frame = tk.Frame(window)
-frame.grid(row=0, column=0, sticky="nsew")
+        # main frame の作成
+        self.frame = tk.Frame(self.window)
+        self.frame.grid(row=0, column=0, sticky="nsew")
 
-# app frameの作成
-frame_app = tk.Frame(frame)
-frame_app.pack(fill=tk.BOTH)
-
-
-# Canvas の作成
-background = tk.PhotoImage(file="AI.png")
-canvas = tk.Canvas(frame, width=754, height=1080, scrollregion=(0, 0, 1080, 1260))
-
-# Canvas上に配置するframeの作成
-frame_canvas = tk.Frame(canvas, background="#000000")
-canvas.create_window((0, 0), window=frame_canvas, anchor=tk.NW)
-canvas.create_image(377, 540, image=background)
+        # app frameの作成
+        self.frame_app = tk.Frame(self.frame)
+        self.frame_app.pack(fill=tk.BOTH)
 
 
-# 水平方向のスクロールバーを作成
-xbar = tk.Scrollbar(frame, orient=tk.HORIZONTAL)
+        # Canvas の作成
+        self.background = tk.PhotoImage(file="AI.png")
+        self.canvas = tk.Canvas(self.frame, width=754, height=1080, scrollregion=(0, 0, 1080, 1260))
 
-# 垂直方向のスクロールバーを作成
-ybar = tk.Scrollbar(frame, orient=tk.VERTICAL)
+        # Canvas上に配置するframeの作成
+        self.frame_canvas = tk.Frame(self.canvas, background="#000000")
+        self.canvas.create_window((0, 0), window=self.frame_canvas, anchor=tk.NW)
+        self.canvas.create_image(377, 540, image=self.background)
 
-# キャンバスの下に水平方向のスクロールバーを配置
-xbar.pack(anchor=NW, fill=X, side=tk.BOTTOM)
 
-# キャンバスの右に垂直方向のスクロールバーを配置
-ybar.pack(anchor=NE, fill=Y, side=tk.RIGHT)
+        # 水平方向のスクロールバーを作成
+        self.xbar = tk.Scrollbar(self.frame, orient=tk.HORIZONTAL)
 
-# canvasの配置
-canvas.pack()
+        # 垂直方向のスクロールバーを作成
+        self.ybar = tk.Scrollbar(self.frame, orient=tk.VERTICAL)
 
-# スクロールバーのスライダーが動かされた時の処理
-xbar.config(command=canvas.xview)
-ybar.config(command=canvas.yview)
+        # キャンバスの下に水平方向のスクロールバーを配置
+        self.xbar.pack(anchor=NW, fill=X, side=tk.BOTTOM)
 
-# スクロールバーをCanvasに関連付け
-canvas.config(xscrollcommand=xbar.set)
-canvas.config(yscrollcommand=ybar.set)
+        # キャンバスの右に垂直方向のスクロールバーを配置
+        self.ybar.pack(anchor=NE, fill=Y, side=tk.RIGHT)
 
-# Canvasの位置の初期化
-canvas.yview_moveto(0)
-canvas.xview_moveto(0)
+        # canvasの配置
+        self.canvas.pack()
 
-# buttonの作成と設置
-reference_attend_data_button = tk.Button(
-    frame_app,
-    text="出席状況",
-    borderwidth=10,
-    padx=40,
-    pady=15,
-    width=12,
-    height=2,
-    relief=RAISED,
-    cursor="hand2",
-    command=maniplation.reference_attend_data,
-)
-reference_attend_data_button.pack(padx=5, pady=10, side=tk.LEFT)
+        # スクロールバーのスライダーが動かされた時の処理
+        self.xbar.config(command=self.canvas.xview)
+        self.ybar.config(command=self.canvas.yview)
 
-# buttonの作成と設置
-take_photo_command_button = tk.Button(
-    frame_app,
-    text="教室撮影",
-    borderwidth=10,
-    padx=40,
-    pady=15,
-    width=12,
-    height=2,
-    relief=RAISED,
-    cursor="hand2",
-    command=maniplation.take_photo_command,
-)
-take_photo_command_button.pack(padx=5, pady=10, side=tk.LEFT)
+        # スクロールバーをCanvasに関連付け
+        self.canvas.config(xscrollcommand=self.xbar.set)
+        self.canvas.config(yscrollcommand=self.ybar.set)
 
-# buttonの作成と設置
-timetable_button = tk.Button(
-    frame_app,
-    text="時間割",
-    borderwidth=10,
-    padx=40,
-    pady=15,
-    width=12,
-    height=2,
-    relief=RAISED,
-    cursor="hand2",
-    command=lambda: sub_frame.SubFrame(window, frame),
-)
-timetable_button.pack(padx=5, pady=10, side=tk.LEFT)
+        # Canvasの位置の初期化
+        self.canvas.yview_moveto(0)
+        self.canvas.xview_moveto(0)
 
-# buttonの作成と設置
-configuration_button = tk.Button(
-    frame_app,
-    text="設定",
-    borderwidth=10,
-    padx=40,
-    pady=15,
-    width=12,
-    height=2,
-    relief=RAISED,
-    cursor="hand2",
-    command=lambda: maniplation.configuration(my_configuration),
-)
-configuration_button.pack(padx=5, pady=10, side=tk.LEFT)
+        # buttonの作成と設置
+        self.reference_attend_data_button = tk.Button(
+            self.frame_app,
+            text="出席状況",
+            borderwidth=10,
+            padx=40,
+            pady=15,
+            width=12,
+            height=2,
+            relief=RAISED,
+            cursor="hand2",
+            command=maniplation.reference_attend_data,
+        )
+        self.reference_attend_data_button.pack(padx=5, pady=10, side=tk.LEFT)
 
-# frame_timetable 作成部
-sub_frame.SubFrame(window, frame)
+        # buttonの作成と設置
+        self.take_photo_command_button = tk.Button(
+            self.frame_app,
+            text="教室撮影",
+            borderwidth=10,
+            padx=40,
+            pady=15,
+            width=12,
+            height=2,
+            relief=RAISED,
+            cursor="hand2",
+            command=lambda: maniplation.take_photo_command(self.background),
+        )
+        self.take_photo_command_button.pack(padx=5, pady=10, side=tk.LEFT)
 
-# 個人用設定クラス作成
-my_configuration = owner.Owner()
+        # buttonの作成と設置
+        self.timetable_button = tk.Button(
+            self.frame_app,
+            text="時間割",
+            borderwidth=10,
+            padx=40,
+            pady=15,
+            width=12,
+            height=2,
+            relief=RAISED,
+            cursor="hand2",
+            command=lambda: sub_frame.SubFrame(self.window, self.frame),
+        )
+        self.timetable_button.pack(padx=5, pady=10, side=tk.LEFT)
 
-# frameを前面にする
-frame.tkraise()
+        # buttonの作成と設置
+        self.configuration_button = tk.Button(
+            self.frame_app,
+            text="設定",
+            borderwidth=10,
+            padx=40,
+            pady=15,
+            width=12,
+            height=2,
+            relief=RAISED,
+            cursor="hand2",
+            command=lambda: maniplation.configuration(self.my_configuration),
+        )
+        self.configuration_button.pack(padx=5, pady=10, side=tk.LEFT)
+
+        # frame_timetable 作成部
+        sub_frame.SubFrame(self.window, self.frame)
+
+        # 個人用設定クラス作成
+        self.my_configuration = owner.Owner()
+
+        # frameを前面にする
+        self.frame.tkraise()
+
+        self.window.mainloop()
 
 # show main window
 if __name__ == "__main__":
-    window.mainloop()
+    main_frame()
---
+++ auto-attendance-check/GUI/reference_attend_frame.py
@@ -0,0 +1,251 @@
+# maniplation.py > reference_attend_frame() により呼び出されるフレーム
+
+import tkinter as tk
+from tkinter.constants import (NE, NW, RAISED, X, Y,)
+from tkinter import ttk
+from tkinter import messagebox
+import toml
+import json
+import paramiko
+
+class ReferenceAttendFrame(tk.Frame):
+    """
+    mainframeの「時間割」 -> subframeの「時間割の新規登録」
+    ボタンが押された時に作成するフレーム
+    """
+
+    def __init__(self, new_window: tk.Toplevel):
+        super().__init__(new_window, width=754, height=680)
+        # main window の作成
+        
+        self.toplevel = new_window
+
+        self.toplevel.grid_rowconfigure(0, weight=1)
+        self.toplevel.grid_columnconfigure(0, weight=1)
+
+
+        # main frame の作成
+        self.frame = tk.Frame(self.toplevel)
+        self.frame.grid(row=0, column=0, sticky="nsew")
+
+        # app frameの作成
+        self.frame_chose = tk.Frame(self.frame)
+        self.frame_chose.pack(fill=tk.BOTH)
+
+        # 科目名のコンボボックスの作成
+        self.subjects = self.read_subjects()
+        self.chose_subject = ttk.Combobox(self.frame_chose, height=8, width=30, values=self.subjects, font=("Times", 14))
+        self.chose_subject.pack(padx=5, pady=10, side=tk.LEFT)
+
+        # 出席状況を見るボタン
+        self.show_button = tk.Button(
+            self.frame_chose,
+            text="出席状況表示",
+            borderwidth=10,
+            padx=15,
+            pady=5,
+            width=12,
+            height=2,
+            relief=tk.RAISED,
+            cursor="hand2",
+            command=lambda: self.show_data(self.chose_subject.get()),
+        )
+        self.show_button.pack(padx=5, pady=10, side=tk.LEFT)
+
+        self.complete_button = tk.Button(
+            self.frame_chose,
+            text="終了",
+            borderwidth=10,
+            padx=15,
+            pady=5,
+            width=12,
+            height=2,
+            relief=tk.RAISED,
+            cursor="hand2",
+            command=self.toplevel.destroy,
+        )
+        self.complete_button.pack(padx=5, pady=10, side=tk.RIGHT)
+
+        # Canvas の作成
+        self.canvas = tk.Canvas(self.frame, width=754, height=1080, scrollregion=(0, 0, 1080, 1260))
+
+        # Canvas上に配置するframeの作成
+        self.frame_canvas = tk.Frame(self.canvas, background="#000000")
+        self.canvas.create_window((0, 0), window=self.frame_canvas, anchor=tk.NW)
+        
+        # 水平方向のスクロールバーを作成
+        self.xbar = tk.Scrollbar(self.frame, orient=tk.HORIZONTAL)
+
+        # 垂直方向のスクロールバーを作成
+        self.ybar = tk.Scrollbar(self.frame, orient=tk.VERTICAL)
+
+        # キャンバスの下に水平方向のスクロールバーを配置
+        self.xbar.pack(anchor=NW, fill=X, side=tk.BOTTOM)
+
+        # キャンバスの右に垂直方向のスクロールバーを配置
+        self.ybar.pack(anchor=NE, fill=Y, side=tk.RIGHT)
+
+        # canvasの配置
+        self.canvas.pack()
+
+        self.grid(row=0, column=0, sticky="nsew")
+    
+    def read_subjects(self) -> list[str]:
+        """
+        時間割ファイルから科目名一覧を求めリストにする
+        """
+        try:
+            with open("./subjects/subjects.toml", "rt", encoding="UTF-8") as fp:
+                data = toml.load(fp)
+        except FileNotFoundError:
+            data = {}
+
+        # subjects.tomlファイルが存在しない場合
+        if not data:
+            messagebox.showerror("エラー", "時間割が登録されていません")
+            self.toplevel.destroy()
+            return
+            
+        subjects = []
+
+        # 科目名をすべて求める
+        i = ""
+        for day in data:
+            for num in data[day]:
+                for i in subjects:
+                    if data[day][num] == i:
+                        break
+                # まだsubjects[]に入っていない科目の場合
+                if data[day][num] != i and data[day][num] != "":
+                    subjects.append(data[day][num])
+
+        return subjects
+
+    def show_data(self, subject: str):
+        if not subject:
+            messagebox.showerror("エラー", "表示させる科目名を選択してください")
+
+        # treeviewの作成
+        self.column = ("num", "name", "attend", "absent", "late", "authorized absence", "none", "error")
+        self.tree = ttk.Treeview(self.canvas, columns=self.column, show="headings")
+
+        # 列の設定
+        self.tree.column("num", width=40, anchor="w", stretch=False)
+        self.tree.column("name", anchor="w", width=150)
+        self.tree.column("attend", anchor="w", width=60)
+        self.tree.column("absent", anchor="w", width=60)
+        self.tree.column("late", anchor="w", width=60)
+        self.tree.column("authorized absence", anchor="w", width=60)
+        self.tree.column("none", anchor="w", width=60)
+        self.tree.column("error", anchor="w", width=60)
+
+        # 列の見出し設定
+        self.tree.heading("num", text="番号")
+        self.tree.heading("name", text="名前")
+        self.tree.heading("attend", text="出席")
+        self.tree.heading("absent", text="欠席")
+        self.tree.heading("late", text="遅刻")
+        self.tree.heading("authorized absence", text="公欠")
+        self.tree.heading("none", text="初期状態")
+        self.tree.heading("error", text="エラー")
+
+        # sshでラズパイから「科目名.json」ファイルを読み込む
+        # raspberrypiのファイルのパスワードファイルの読み込み
+        with open("raspberrypi_key.toml", mode="rt", encoding="UTF-8") as fp:
+            data = toml.load(fp)
+        
+        # SSH接続
+        client = paramiko.SSHClient()
+        client.load_system_host_keys()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        
+        try:
+            client.connect(data["IP_ADDRESS"], username=data["USER_NAME"], password=data["PASS_WORD"], timeout=3.9)
+        except Exception:
+            messagebox.showerror("エラー", "ラズパイに接続できませんでした")
+            return
+        subject="keisanki"
+        try:
+            # 指定した科目のファイルを読み込む
+            sftp_connection = client.open_sftp()
+            sftp_connection.get(f"/home/pi/aac/attend_data/json/{subject}.json", f"attend_data/{subject}.json")
+        except Exception as e:
+            messagebox.showerror("エラー", "ファイルを取得できませんでした")
+            client.close()
+            return
+        
+        # 出席・欠席...の回数を計算
+        attend_data = self.culcu_num(subject)
+        i = 0
+        for list in attend_data:
+            self.tree.insert(parent="", index="end", iid=i, values=list)
+            i += 1
+
+        # スクロールバーのスライダーが動かされた時の処理
+        self.xbar.config(command=self.tree.xview)
+        self.ybar.config(command=self.tree.yview)
+
+        # スクロールバーをCanvasに関連付け
+        self.tree.config(xscrollcommand=self.xbar.set)
+        self.tree.config(yscrollcommand=self.ybar.set)
+
+        # Canvasの位置の初期化
+        self.tree.yview_moveto(0)
+        self.tree.xview_moveto(0)
+
+        self.tree.pack(pady=20, side=tk.LEFT)
+
+    def culcu_num(self, subject: str) -> list:
+        """
+        出席・欠席・遅刻・...の回数を計算
+
+        出席の正体を表す列挙体
+        Attributs
+        ---------
+        ATTEND : str
+            出席
+        ABSENCE : str
+            欠席
+        LATENESS : str
+            遅刻
+        AUTHORIZEED_ABSENCE : str
+            公欠
+        NONE : str
+            初期状態、未確認状態
+        ERROR : str
+            エラー
+        """
+
+        ATTEND = "attend"
+        ABSENCE = "absence"
+        LATENESS = "lateness"
+        AUTHORIZEED_ABSENCE = "authorized absence"
+        NONE = "none"
+        ERROR = "error"
+        
+        # 読み込んだファイルから出席数・欠席数・等を計算
+        with open(f"attend_data/{subject}.json", mode="rt", encoding="UTF-8") as fp:
+            data = json.load(fp)
+        
+        attend_data = []
+
+        for item in range(0,len(data)):
+            count = [0]*6
+            for num in data[item]["attendance states"]:
+                if num == ATTEND:
+                    count[0] += 1
+                if num == ABSENCE:
+                    count[1] += 1
+                if num == LATENESS:
+                    count[2] += 1
+                if num == AUTHORIZEED_ABSENCE:
+                    count[3] += 1
+                if num == NONE:
+                    count[4] += 1
+                if num == ERROR:
+                    count[5] += 1
+            attend_data.append(
+                (data[item]["student number"], data[item]["name"], *count)
+            )
+        
+        return attend_data
